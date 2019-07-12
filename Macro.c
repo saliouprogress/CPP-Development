c62ebdecce3010de7386b672f53f2fc215306133
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-12 11:00:38 -0400

    Update Macro.c

--- Macro.c
+++ Macro.c
@@ -12,3 +12,15 @@ scanf("%d %d", &a, &b);
 printf("\n%d %d", a,b);
 return 0;
 }
+///////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h> 
+ #define MACRO(x)  x * (x+5)
+ int main(void)
+ { 
+#ifdef MACRO // test whether MACRO1 is defined...    
+ printf("\nMACRO Defined...\n"); 
+#endif     
+int res = MACRO(2);     
+printf("\n res = [%d]\n", res);    
+ return 0;
+ }
