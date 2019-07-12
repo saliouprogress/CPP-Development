c89fba7b7e8fc62c1b3965bedaf90a0634c096ba
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-12 10:38:20 -0400

    Update Macro.c

--- Macro.c
+++ Macro.c
@@ -1,27 +1,14 @@
     
 #include <stdio.h>
 
-//#include DEFS_H
-#define PRINT1
-#define PRINT2
-
-
+//#include "Defs.h"
 
 int main()
 {
 
 int a, b;
 scanf("%d %d", &a, &b);
-//int TEST();
+
 printf("\n%d %d", a,b);
-
-#ifdef PRINT1 // test whether MACRO1 is defined...
-printf("\nMACRO1 Defined %d\n", a);
-#endif
-
-#ifdef PRINT2 // test whether MACRO2 is defined...
- printf("\nMACRO2 Defined %d , %d\n", a,b);
-#endif
-
 return 0;
 }
