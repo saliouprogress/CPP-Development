2b447881d6021539e7ccd0ab8bb71b57db5649a6
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-12 10:30:27 -0400

    Create Defs.h

---
+++ Defs.h
@@ -0,0 +1,16 @@
+    
+#include <stdio.h>
+
+#include DEFS_H
+#define DEFS_H
+
+#define PRINT1
+#define PRINT2
+
+#ifdef PRINT1 // test whether MACRO1 is defined...
+printf("\nMACRO1 Defined %d\n", a);
+#endif
+
+#ifdef PRINT2 // test whether MACRO2 is defined...
+ printf("\nMACRO2 Defined %d , %d\n", a,b);
+#endif
