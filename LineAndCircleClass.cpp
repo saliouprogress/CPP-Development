3305ffd25eb95e1f7482d91509fa8c3c4f4134d4
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-23 15:49:28 -0400

    Update LineAndCircleClass.cpp

--- LineAndCircleClass.cpp
+++ LineAndCircleClass.cpp
@@ -1,9 +1,10 @@
+//////////////////////////////////////// Point Class /////////////////////////////////////
+
 #ifndef POINT_H             ///////////////  http://www.cplusplus.com/forum/general/124555/
 #define POINT_H
 #include <iostream>
 
 using namespace std;
-
 class Point
 {
 public:
@@ -23,7 +24,7 @@ private:
 
 #endif // POINT_H 
 
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// Line Class ////////////////////////////////////
 
 #ifndef LINE_H
 #define LINE_H
@@ -49,7 +50,120 @@ private:
 
 #endif // LINE_H 
 
-/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// Circle Class ///////////////////////////////////////
+
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include <iostream>
+#include <sstream>
+#include <string>
+//#include "point.h"
+
+using namespace std;
+
+class Circle
+{
+public:
+    Circle();
+    Circle(Point P1, Point P2);
+    double GetXC();
+    double GetYC();
+    //CenterPoint();
+    double Radius();
+    // Diameter();
+    //Area();
+    //Circumference();
+    string ToString(Point P1, double radius);
+private:
+    Point Center_P, Edge_P;
+    double Center_x, Center_y, Edge_x, Edge_y;
+    double m_radius;
+};
+
+#endif // CIRCLE_H 
+
+///////////////////////////////////// Circle.cpp //////////////////////////////////////////
+
+//#include "line.h"
+//#include "point.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+//using namespace std;
+
+//Default constructor
+Circle::Circle():Center_x(0),Center_y(0), m_radius(0)
+{
+    //left blank intentionally
+}
+
+Circle::Circle(Point P1, Point P2)
+{
+    Center_P = P1;
+    Edge_P = P2;
+    
+    //double x1,x2,y1,y2;
+    Center_x = P1.GetX();
+    Edge_x = P2.GetX();
+    Center_y = P1.GetY();
+    Edge_y = P2.GetY();
+    /*
+    //derived from slope(m) intercept(b) form
+    double dx,dy;
+    
+    dx = Edge_x - Center_x;
+    dy = Edge_y - Center_y;
+    
+    m_radius = std::sqrt(pow(dx,2) + pow(dy,2));
+    */
+    double area, circom, diam;
+    area = M_PI*pow(m_radius,2);
+    
+    circom = 2 * M_PI * m_radius;
+    diam = 2 * m_radius;
+    cout << Center_x << "," << Center_y << ", Radius "<< m_radius << ", Area " << 
+        area <<  ", Circ " << circom << ", Diam: "<< diam << endl;
+}
+
+double Circle::Radius()
+{
+    //derived from slope(m) intercept(b) form
+    double dx,dy;
+    
+    dx = Edge_x - Center_x;
+    dy = Edge_y - Center_y;
+    
+    m_radius = std::sqrt(pow(dx,2) + pow(dy,2)); 
+    cout << m_radius;
+    return m_radius;
+}
+
+//ACCESSORS
+double Circle::GetXC()
+{
+    return Center_x;
+    
+}
+double Circle::GetYC()
+{
+    return Center_y;
+}
+/*
+std::string Circle::ToString(Circle Center_P, Edge_P)
+{
+    std::string out_string;
+    std:stringstream buffer;
+    
+    buffer << "Resulting Circle: " << "Radius = " << m_radius << "x coord = " << endl;
+    out_string = buffer.str();
+    cout << out_string;
+    return out_string;
+}  */
+
+
+//////////////////////////////////// Line.cpp File /////////////////////////////////////////
 
 //#include "line.h"
 //#include "point.h"
@@ -102,16 +216,15 @@ double Line::GetB()
 
 std::string Line::ToString(double A, double B)
 {
-    cout << "works" << endl;
     std::string out_string;
     std:stringstream buffer;
     
-    cout << "Resulting line: " << "y = " << A << "x" << " + " << B << endl;
+    buffer << "Resulting line: " << "y = " << A << "x" << " + " << B << endl;
     out_string = buffer.str();
     cout << out_string;
     return out_string;
 }
-///////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Point.cpp ///////////////////////////////////////////
 
 //#include "point.h"
 //#include <iostream>
@@ -168,7 +281,7 @@ using namespace std;
 
 int main()
 {
-
+    
     Point Point1_Object(5,5);
     cout << "Point1 (x,y) is: (" << Point1_Object.GetX() << "," << Point1_Object.GetY() << ")" << endl;
     cout<<endl;
@@ -179,8 +292,12 @@ int main()
     Line THE_Line(Point1_Object, Point2_Object);
     //cout<< "Resulting line: " << "y = " << THE_Line.GetA() << "x" << " + " << THE_Line.GetB() << endl;
     THE_Line.ToString(THE_Line.GetA(), THE_Line.GetB());
-
-
+    
+    //double radius = 5;
+    Circle THE_Circle(Point1_Object, Point2_Object);
+    //THE_Circle.ToString(THE_Circle.GetXC(),, );
+        
+        
     cout << endl << endl << endl << "END!!!" << endl;
     return 0;
 }
