984b6b016d1c4c4552196202098e34c3b4527f8a
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-25 16:02:09 -0400

    Update BirthdayCompositonClass.cpp

--- BirthdayCompositonClass.cpp
+++ BirthdayCompositonClass.cpp
@@ -2,23 +2,67 @@
 #include <string>
 
 using namespace std;
+class Point1{
 
+public:
+    Point1() {
+      m_x1 = 0;   m_y1 = 0;
+      cout << "Point1 Default Constructor." << endl; 
+    }
+    
+    Point1(int x1, int y1) {
+      m_x1 = x1;   m_y1 = y1;
+      cout << "Point1 Start Point Constructor." << endl; 
+    }
+    
+    Point1(const Point1& old) {
+      m_x1 = old.m_x1;   m_y1 = old.m_y1;
+      cout << "Point1 Copy Constructor." << endl;
+    }
+    
+    ~Point1() {  //cout << "Birtthday Destructor." << endl; 
+                }
+    
+    void X(int x1) { m_x1 = x1; }
+    
+    void Y(int y1) { m_y1 = y1; }
+      
+    int X() {  return m_x1; }
+    
+    int Y() { return m_y1; }
+    
+    
+    void Distance1()
+    {
+        int distance =  m_x1 - m_y1;
+        cout << distance << endl;
+    }
+    void ToString1() { cout<< m_x1 <<"/" <<m_y1 << endl; }
+    
+private:
+    int m_x1; int m_y1;
+    
+};
 class Birthday{
 
 public:
     Birthday() {
       month = 0;   day = 0;   year = 0;
-      cout << "Birthday Default Constructor." << endl; }
+      cout << "Birthday Default Constructor." << endl; 
+    }
     
     Birthday(int xmonth, int yday, int zyear) {
       month = xmonth;   day = yday;   year = zyear;
-      cout << "Birthday constructor with Start and End Point." << endl; }
+      //cout << "Birthday constructor with Start and End Point." << endl; 
+    }
     
     Birthday(const Birthday& old) {
       month = old.month;   day = old.day;    year = old.year;
-      cout << "Birtthday Copy Constructor." << endl;}
+      //cout << "Birtthday Copy Constructor." << endl;
+    }
     
-    ~Birthday() {  cout << "Birtthday Destructor." << endl; }
+    ~Birthday() {  //cout << "Birtthday Destructor." << endl; 
+                }
     
     void X(int x) { month = x; }
     
@@ -32,37 +76,65 @@ public:
     
     int Z() { return year; }
     
-    void ToString(){ cout<< month <<"/" <<day <<"/" << year <<endl; }
+    void Distance2()
+    {
+        int distance =  month - day + year;
+        cout << distance << endl;
+        //return distance;
+    }
+    void ToString2(){ cout<< month <<"/" <<day <<"/" << year <<endl; }
     
 private:
     int month; int day; int year;
+    
 };
 
 class People{
 public:
-    People(string cname, Birthday cdateOfBirth) :name(cname), dateOfBirth(cdateOfBirth)
-    { cout << "People Constructor with Start Point." << endl; }
+    People() { cout << "People Default Constructor." << endl;}
     
+    People(string cname, Birthday cdateOfBirth) :name(cname), dateOfBirth(cdateOfBirth)
+    { cout << "People Constructor with End Point." << endl; }
+    
+    People(string cname, Point1 cObject) :name(cname), Object(cObject)
+    { cout << "People Constructor with Start Point." << endl; }
+   
     ~People() { cout << "People Destructor." << endl; }
     
-    void ToString() { cout<<name <<" was born on: "; dateOfBirth.ToString(); }
+    void Length()
+    {  
+        cout<< name << " The distance is: "; dateOfBirth.Distance2(); 
+        cout<< name << " The distance is: "; Object.Distance1(); 
+    }
+    
+    void ToString() { 
+        cout<<name <<" was born on: "; dateOfBirth.ToString2();
+        cout<<name <<" was born on: "; Object.ToString1(); }
     
 private:
-    string name;   Birthday dateOfBirth;
+    string name; Point1 Object; Birthday dateOfBirth; 
 };
 
-int main() {
-    int x,y,z;
-    cin >> x; cin >> y; cin >> z;
+int main() 
+{
+    int x,y;
+    cin >> x; cin >> y;
     
-    Birthday Object;
-    Object.X(x); Object.Y(y); Object.Z(z);
+    int a,b,c;
+    cin >> a; cin >> b; cin >> c;
     
-    Birthday birthObject(8,9,97);
+    Point1 StartPoint(x,y);
+    Birthday EndPoint(a,b,c);
+    cout << endl << endl;
     
-    People infoObject("Lenny the Cowboy", birthObject);
+    People infoObject("Start Point Information is: ",StartPoint);
     infoObject.ToString();
-  
-    People IObject("Lenny the Cowboy", Object);
+    infoObject.Length();
+    
+    cout << endl << endl;
+    
+    
+    People IObject("End Point Information is: ", EndPoint);
     IObject.ToString();
+    IObject.Length();
 }
