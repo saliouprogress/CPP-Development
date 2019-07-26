4d5ea9beca95ec71d5f89a5fd4eac9210447f783
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-26 19:10:44 -0400

    Create StairCaseBlocks.cpp

---
+++ StairCaseBlocks.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int N, totalBlocks = 1; 
+	cin >> N;
+	int remainingBlocks = N;
+ 
+	while (totalBlocks <= remainingBlocks)
+	{
+          	remainingBlocks = remainingBlocks - totalBlocks; 
+		cout << totalBlocks << ", " << remainingBlocks << endl;
+        	totalBlocks++; 
+	}  	
+	return 0;
+}
