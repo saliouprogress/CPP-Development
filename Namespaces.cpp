5d7cb5cab88e6ea9786369b0c09daaf9e157e5c5
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-07-31 13:43:09 -0400

    Update Namespaces.cpp

--- Namespaces.cpp
+++ Namespaces.cpp
@@ -1,1 +1,52 @@
 
+namespace MamadouDiallo 
+{
+  namespace CAD { int a = 5; void display(); }
+  namespace Containers {int a = 5; void display();}
+}
+
+// A C++ code to demonstrate that we can define 
+// methods outside namespace. 
+#include <iostream> 
+using namespace std; 
+
+// Creating a namespace 
+namespace ns 
+{ 
+	void display(); 
+	class geek 
+	{ 
+	public: 
+	void display(); 
+	}; 
+} 
+
+// Defining methods of namespace 
+void ns::geek::display() 
+{ 
+	cout << "ns::geek::display()\n"; 
+} 
+void ns::display() 
+{ 
+	cout << "ns::display()\n"; 
+} 
+void MamadouDiallo::CAD::display() 
+{ 
+	cout << "MamadouDiallo::CAD::display()\n"; 
+} 
+
+void MamadouDiallo::Containers::display() 
+{ 
+	cout << "MamadouDiallo::Containers::display()\n"; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	ns::geek obj; 
+	ns::display(); 
+	obj.display(); 
+  MamadouDiallo::CAD::display();
+  MamadouDiallo :: Containers :: display();
+	return 0; 
+} 
