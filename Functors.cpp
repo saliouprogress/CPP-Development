f935e422cb6c0fc23f2285b671dde518ee60e3ab
saliouprogress <49839551+saliouprogress@users.noreply.github.com>
2019-09-03 15:02:08 -0400

    Update Functors.cpp

--- Functors.cpp
+++ Functors.cpp
@@ -12,19 +12,19 @@ private:
 public: 
     isSmaller(T n) : num(n) {  } 
     bool operator () (T arr_num) const { 
-        return num >= arr_num ? true : false;
+        return (arr_num <= num) ? true : false;
     } 
 }; 
   
 // Driver code 
 int main() 
 { 
-   std::vector<double> myvector;
-  for (int i=1; i<10; i++) myvector.push_back(i*1.5); // myvector: 1 2 3 4 5 6 7 8 9
-  double cutoff = 6;
+  std::vector<double> myvector;
+  for (int i=1; i<20; i++) myvector.push_back(-i*.5); // myvector: 1 2 3 4 5 6 7 8 9
+  for (int i=0; i<myvector.size(); i++) cout << myvector[i] << " " << endl; 
+  double cutoff = -2;
   double mycount = count_if(myvector.begin(), myvector.end(), isSmaller<double>(cutoff));
-  std::cout << "myvector contains " << mycount  << " odd values.\n";
+  std::cout << "myvector contains " << mycount  << " values less than or equal to " << cutoff << ".\n";
   
-  for (int i=0; i<myvector.size(); i++) 
-    cout << myvector[i] << " "; 
+  
 } 
